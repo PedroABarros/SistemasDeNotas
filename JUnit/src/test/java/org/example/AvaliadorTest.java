4a127ae0b3ceaa823da1faefac41c9b26677201b
Pedro Barros <pedrokii.a.b@gmail.com>
2024-05-09 16:36:04 -0300

    finalizando os casos de teste

--- JUnit/src/test/java/org/example/AvaliadorTest.java
+++ JUnit/src/test/java/org/example/AvaliadorTest.java
@@ -1,9 +1,19 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AvaliadorTest {
+
+    @Test
+    void deveRetornarValorInvalido() {
+        Avaliador nota = new Avaliador();
+        assertThrows(IllegalArgumentException.class, () -> {
+            nota.determinarNotaAluno(-1);
+        });
+    }
 
     @Test
     void deveRetornarF() {
